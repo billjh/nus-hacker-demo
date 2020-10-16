39971f69682f4c278823a907b970bace7c150399
Jiang Huan <huan.jiang@titansoft.com.sg>
2020-10-16 13:43:54 +0800

    init commit

---
+++ app-v1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var id int
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi, NUS Hackers! (%d)", id)
+}
+
+func main() {
+	// generate a random id
+	rand.Seed(time.Now().UnixNano())
+	id = rand.Intn(9999)
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
